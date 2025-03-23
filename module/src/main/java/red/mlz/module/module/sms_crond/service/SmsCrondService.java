00803d08b2120c5f52bb59f75a5ee88c5e1e3b8f
xiaobai115@qq.com <3348836046@qq.com>
2025-03-24 00:40:17 +0800

    线程关闭

--- module/src/main/java/red/mlz/module/module/sms_crond/service/SmsCrondService.java
+++ module/src/main/java/red/mlz/module/module/sms_crond/service/SmsCrondService.java
@@ -60,8 +60,8 @@ public class SmsCrondService {
                 smsTask.setIsDeleted(0);  // 默认未删除
                 mapper.insert(smsTask);
             });
-
         }
+        executorService.shutdown();
 
     }
 
