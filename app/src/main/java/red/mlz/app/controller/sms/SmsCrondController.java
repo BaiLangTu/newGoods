bc475b8514e03fb326eb322511190e20f19ae4e1
xiaobai115@qq.com <3348836046@qq.com>
2025-03-24 00:37:09 +0800

    三种发送短信方式

--- app/src/main/java/red/mlz/app/controller/sms/SmsCrondController.java
+++ app/src/main/java/red/mlz/app/controller/sms/SmsCrondController.java
@@ -1,9 +1,6 @@
 package red.mlz.app.controller.sms;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +13,6 @@ public class SmsCrondController {
     @Autowired
     private SmsCrondService smsCrondService;
 
-    @PostMapping("/send")
-    public ResponseEntity<String> sendSms(@RequestParam String phoneNumbers) {
-        try {
-            // 调用发送短信的服务方法
-            smsCrondService.sendThread(phoneNumbers);
-            return ResponseEntity.ok("短信发送请求已接收，正在处理中...");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("短信发送失败: " + e.getMessage());
-        }
-    }
     // 多线程发送
     @RequestMapping("send/thread")
     public Response sendThread(@RequestParam(name = "phone") String phone){
